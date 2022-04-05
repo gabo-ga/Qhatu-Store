51ee3cc6828eecb7e05c6e70e2bca61b712b50b7
Luis <rosalescaero@gmail.com>
2022-04-05 18:49:37 -0400

    se ha limpiado el App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import SupervisorPage from "./pages/supervisorProfile/supervisorProfile";
-import Footer from "./components/fotter/footer";
-import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
-      <SupervisorPage></SupervisorPage>
-      <Footer></Footer>
+      Qhatu store
     </div>
   );
 }
