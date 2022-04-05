9cfb2740cf3c7a0656e5950ada1c43ce4127eaf9
gary <garymamani12@gmail.com>
2022-04-04 21:20:06 -0400

    fix: lang

--- src/components/FirebaseConfig.js
+++ src/components/FirebaseConfig.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
-const firebaseConfiguracion = {
+const firebaseConfig = {
   apiKey: 'AIzaSyDUuVEv-hcIC0htFT4nnETtEYDQp7R7ORM',
   authDomain: 'qhatu-store.firebaseapp.com',
   projectId: 'qhatu-store',
@@ -11,9 +11,9 @@ const firebaseConfiguracion = {
   measurementId: 'G-RZTW6GTGLE'
 };
 
-const aplicacion = initializeApp(firebaseConfiguracion);
-const baseDeDatos = getFirestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
 
 //Registrar usuarios, reemplazar parametros por los recibidos
-const registrar = (parametros)=>
-  addDoc(collection(baseDeDatos,'usuario'),{parametros});
+const register = (params)=>
+  addDoc(collection(db,'user'),{params});
