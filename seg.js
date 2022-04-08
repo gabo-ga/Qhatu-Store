dc3ae93386a01f922b3047e416d99ba8f2d6fcea
Luis <rosalescaero@gmail.com>
2022-04-08 13:09:03 -0400

    fix: eliminados archivos adicionales

--- seg.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementsByClassName('form-register');
-const inputs =document.querySelectorAll('#form-register input');
-const expresions= {
-    names:/^([a-z\.]){1,40}$/,
-    emails: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    phone: /^\d{7,8}/,
-    password:/^.{4,12}$/
-}
