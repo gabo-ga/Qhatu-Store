d87824c01d814ed4a33b70161d712cbe6bceb4a6
Luis <rosalescaero@gmail.com>
2022-05-02 21:12:55 -0400

    fix: errores en estilos para la vista item comany

--- src/components/listItem/listCompany.jsx
+++ src/components/listItem/listCompany.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { onGetCompanies, deleteCompany } from '../FirebaseConfig';
-import { MdMode } from "react-icons/md";
 
 import { Link } from 'react-router-dom';
 import './listItem.css'
 import './listCompany.css'
 
 import "../modal/companyModal.css"
+import "./empItemList.css"
 
 const ListCompany = () => {
   const [count, setCount] = useState(0);
@@ -23,6 +23,7 @@ const ListCompany = () => {
       })))
     });
   },[]);
+
   const ColorSelect = (props) => {
     if (props%2 > 0){
       tableNumber += 1
@@ -37,35 +38,31 @@ const ListCompany = () => {
       deleteCompany(id);
   }
 
-
-
   return (
     <div className="container-fluid p-0">
       {companyData?.map(({id,data}) =>( 
-        <div className={ ColorSelect(tableNumber) ?"item-list-table row" : "item-list-table2 row"}>
-          <div className="col-4 item-list-id">
-                <p> <strong>Empresa: </strong> {data.name}</p>     
-          </div>
-          <div class= "buttons g-col-4 d-flex">
-            <div className="col-lg-2 col-3 d-flex align-items-center buttonItem">
-                <button type='button' class="btn" data-bs-toggle="modal" data-bs-target="#editEmp">
-                    <img className='img-edit' 
-                    src="https://firebasestorage.googleapis.com/v0/b/qhatu-store.appspot.com/o/lapiz.png?alt=media&token=9e834e4f-8b4d-42af-bf4a-ef08deae3cd0" 
-                    alt="" />
-                </button>
-                </div>
-                <div className="col-lg-2 col-3 d-flex align-items-center">
-                <button  type='button' class="btn" data-bs-toggle="modal" data-bs-target="#delEmp">
-                    <img className='img-del'
-                    src="https://firebasestorage.googleapis.com/v0/b/qhatu-store.appspot.com/o/basurero.png?alt=media&token=5378bed1-9615-4b17-8d70-942ed0829678"
-                    alt="" />
-                </button>
-                </div>
-            </div>
+        <div className={ ColorSelect() ?"emp-item-list-1 row p-0" : "emp-item-list-2 row p-0"}>
+        <div className="col-6 col-lg-8 d-flex align-items-center">
+            <p className='m-0 item-name'>Empresa: {data.name}</p>
+        </div>
+        <div className="col-lg-2 col-3 d-flex align-items-center">
+          <button type='button' class="btn" data-bs-toggle="modal" data-bs-target="#editEmp">
+            <img className='img-edit' 
+            src="https://firebasestorage.googleapis.com/v0/b/qhatu-store.appspot.com/o/lapiz.png?alt=media&token=9e834e4f-8b4d-42af-bf4a-ef08deae3cd0" 
+            alt="" />
+          </button>
+        </div>
+        <div className="col-lg-2 col-3 d-flex align-items-center">
+          <button  type='button' class="btn" data-bs-toggle="modal" data-bs-target="#delEmp">
+            <img className='img-del'
+            src="https://firebasestorage.googleapis.com/v0/b/qhatu-store.appspot.com/o/basurero.png?alt=media&token=5378bed1-9615-4b17-8d70-942ed0829678"
+            alt="" />
+          </button>
+        </div>
 
-            <div class="modal fade " id="delEmp" tabindex="-1" aria-labelledby="delEmp" aria-hidden="true">
+        <div class="modal fade " id="delEmp" tabindex="-1" aria-labelledby="delEmp" aria-hidden="true">
                 <div className='d-flex d-flex justify-content-center'>
-                <div class="modal-dialog modal-body-edit">
+                <div class="modal-dialog modal-body-edit d-flex justify-content-center">
                   <div class="modal-content modal-body-edit">
                     <div class="modal-body  d-flex justify-content-center">
                     <div className='modal-del-container'>
@@ -85,7 +82,7 @@ const ListCompany = () => {
                           </div>
                           <div className='col-12 d-flex justify-content-evenly modal-del-btns'>
                               <button type="button" class="btn modal-del-btn" data-bs-dismiss="modal"
-                              onClick={() => handleClick(id)}
+                              onClick={() => handleClick()}
                               >Eliminar</button>
                               <button type="button" class="btn modal-del-btn" data-bs-dismiss="modal">Cancelar</button>
                             </div>
@@ -97,11 +94,10 @@ const ListCompany = () => {
 
                 </div>
         </div>
-
-            <div class="modal fade " id="editEmp" tabindex="-1" aria-labelledby="editEmp" aria-hidden="true">
+        <div class="modal fade " id="editEmp" tabindex="-1" aria-labelledby="editEmp" aria-hidden="true">
                 <div className='d-flex justify-content-center'>
-                <div class="modal-dialog modal-body-del ">
-                  <div class="modal-content modal-body-del">
+                <div class="modal-dialog modal-body-del d-flex justify-content-center">
+                  <div class="modal-content modal-body-del ">
                     <div class="modal-body  d-flex justify-content-center">
                     <div className='modal-del-container'>
                         <div className='row'>
@@ -119,7 +115,7 @@ const ListCompany = () => {
                            </p>
                           </div>
                           <div className='col-12 d-flex justify-content-evenly modal-del-btns'>
-                            <Link to= {`/administrator/editcompany/${id}`}> 
+                            <Link to={`/administrator/editcompany/${id}`}> 
                               <button type="button" class="btn modal-del-btn " data-bs-dismiss="modal"
                               >Editar</button>
                             </Link>
@@ -132,9 +128,8 @@ const ListCompany = () => {
                 </div>
 
                 </div>
-
-            </div>
         </div>
+    </div>
       ))}
     </div>
   );
