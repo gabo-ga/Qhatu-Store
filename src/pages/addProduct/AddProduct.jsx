00556364bb88560fa203190c0f6d76b8a96079d7
Luis <rosalescaero@gmail.com>
2022-05-22 22:30:04 -0400

    fix: espacio en blanco para formulario de registro de producto

--- src/pages/addProduct/AddProduct.jsx
+++ src/pages/addProduct/AddProduct.jsx
@@ -9,7 +9,7 @@ const AddProduct = () => {
             <div >
                 <h1 className="titleQhatu text-center">Qhatu Store</h1>
             </div>
-            <div className="align-middle">
+            <div className="align-middle addproduct-form-container">
                 <Form></Form>
             </div>
         </div>
