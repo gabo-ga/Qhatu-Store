03c9a8f74f8593e0494cb5c63c9d3ef451489cd2
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2022-05-25 15:18:11 -0400

    feat: responsive desing de la pantalla seller

---
+++ src/pages/sellerMain/sellerMain.jsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react';
+import AddOrderTable from '../addOrderTable/AddOrderTable';
+import { useProduct  } from '../../components/context/products';
+import { useNavigate } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { registerOrder } from '../FirebaseConfig';
+
+import "./sellerMainView.css"
+
+const sellerMain = () => {
+    return(
+        <body>
+            <header>
+                <nav>
+                    <h1>TODO LO QUE NECESITAS</h1>
+                </nav>
+            </header>
+            <main>
+                <div>
+                    <h2>Bienvenido</h2>
+                    <h2>Seleccione una marca para elegir productos</h2>
+                </div>
+                <div className='btn-colection'>
+                <button type="button" onclick="alert('helllo')">
+                     <img src="https://firebasestorage.googleapis.com/v0/b/qhatu-store.appspot.com/o/cocacola-logo.jpg?alt=media&token=eda01c06-b9de-40be-89a0-c6ef12665e04"></img>
+                </button>
+                <button type="button"> <img src="https://firebasestorage.googleapis.com/v0/b/qhatu-store.appspot.com/o/cocacola-logo.jpg?alt=media&token=eda01c06-b9de-40be-89a0-c6ef12665e04" ></img></button>
+                <button type="button"> <img src="https://firebasestorage.googleapis.com/v0/b/qhatu-store.appspot.com/o/cocacola-logo.jpg?alt=media&token=eda01c06-b9de-40be-89a0-c6ef12665e04" ></img></button>
+                <button type="button"> <img src="https://firebasestorage.googleapis.com/v0/b/qhatu-store.appspot.com/o/cocacola-logo.jpg?alt=media&token=eda01c06-b9de-40be-89a0-c6ef12665e04"></img></button>
+                <button type="button"> <img src="https://firebasestorage.googleapis.com/v0/b/qhatu-store.appspot.com/o/cocacola-logo.jpg?alt=media&token=eda01c06-b9de-40be-89a0-c6ef12665e04" ></img></button>
+                <button type="button"> <img src="https://firebasestorage.googleapis.com/v0/b/qhatu-store.appspot.com/o/cocacola-logo.jpg?alt=media&token=eda01c06-b9de-40be-89a0-c6ef12665e04"></img></button>
+                </div>
+            </main>
+        </body>
+    );
+}
+
+export default sellerMain;
